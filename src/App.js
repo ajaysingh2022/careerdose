4ae343778ee789bdb58ed1d813d21c6ab91b684b
ajaysingh2022 <ajay.singh.dev@webologixglobal.com>
2022-09-22 16:20:53 +0530

    css done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Header from './Component/Header/index'
 import Homepage from './Component/Homepage/index'
-import Accur from "./Component/Accup/index"
 import { Routes, Route } from "react-router-dom";
 import After10 from './Component/CareerGuidance/index'
 import StudyAbroad from './Component/CareerGuidance/StudyAbroad'
@@ -9,19 +7,11 @@ import StudyAbroad from './Component/CareerGuidance/StudyAbroad'
 function App() {
   return (
     <>
-      {/* <Routes> */}
-      {/* <div className='App'>
-      <Header />
-      <Homepage />
-      </div>
-      <div>
-        <Accur />
-      </div> */}
-      <div>
-        {/* <After10 /> */}
-        <StudyAbroad />
-      </div>
-      {/* </Routes> */}
+      <Routes>      
+      <Route path={'/'} element= {<Homepage />} />
+      <Route exact path={'/studyabroad'} element= {<StudyAbroad />} />
+      <Route exact path={'/after10'} element= {<After10 />} />
+      </Routes>
     </>
   );
 }
